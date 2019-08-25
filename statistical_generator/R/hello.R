f8a4dc79320a57791645cdace13448f9588ef402
Alexandre de Souza <alexandre@aledsz.com.br>
2019-08-25 00:48:06 -0300

    Updated R script

--- statistical_generator/R/hello.R
+++ statistical_generator/R/hello.R
@@ -17,5 +17,9 @@ args = commandArgs(trailingOnly=TRUE)
 if (length(args) == 0) {
 	cat("there's no args")
 } else {
-	cat(args[1])
+	base64_json = jsonlite::base64_dec(args[1])
+	json = jsonlite::fromJSON(rawToChar(base64_json))
+	json$test = "321"
+	json$processed_by = "R application"
+	cat(jsonlite::base64_enc(jsonlite::toJSON(json, auto_unbox = TRUE)))
 }
