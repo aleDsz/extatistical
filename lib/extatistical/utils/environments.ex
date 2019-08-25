04d3a8db75ae8669cb30f1ad2d068eb067ffa6cf
Alexandre de Souza <alexandre@aledsz.com.br>
2019-08-25 00:46:59 -0300

    Added module to handle with environment variables

---
+++ lib/extatistical/utils/environments.ex
@@ -0,0 +1,28 @@
+defmodule Extatistical.Utils.Environments do
+  @moduledoc """
+  Handle environment variables from config based on pattern matching
+  # Examples
+  iex> Environments.get_env({:system, "ENV_VAR"})
+  "42 is the answer"
+  iex> Environments.get_json_env({:system, "ENV_VAR2"})
+  %{"answer" => "42 is the answer"}
+  """
+
+  @doc """
+  Return environment variable from config based on pattern matching
+  """
+  @spec get_env(any) :: any
+  def get_env(env_var) when is_integer(env_var), do: env_var
+  def get_env(env_var) when is_binary(env_var), do: env_var
+  def get_env({:system, env_var}), do: get_env(System.get_env(env_var))
+  def get_env(env_var), do: env_var
+
+  @doc """
+  Return environment variable as JSON from config based on pattern matching
+  """
+  @spec get_json_env(any) :: false | nil | true | binary | [any] | number | map
+  def get_json_env(env_var) when is_integer(env_var), do: %{}
+  def get_json_env(env_var) when is_binary(env_var), do: Poison.decode!(env_var)
+  def get_json_env({:system, env_var}), do: Poison.decode!(get_env(System.get_env(env_var)))
+  def get_json_env(_env_var), do: %{}
+end
