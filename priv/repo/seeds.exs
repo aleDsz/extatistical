2da9460010cbe926f10f0af7a27a148ae8e5bc17
Alexandre de Souza <alexandre@aledsz.com.br>
2019-08-25 00:47:49 -0300

    Added database seeds

---
+++ priv/repo/seeds.exs
@@ -0,0 +1,55 @@
+alias Extatistical.Modules.BadJokes
+
+{:ok, _} = BadJokes.create %{
+  creator: "aleDsz",
+  joke: "Welcome to the Django",
+  score: -2
+}
+
+{:ok, _} = BadJokes.create %{
+  creator: "rafiusky",
+  joke: "Espada de @Xunjin",
+  score: -8
+}
+
+{:ok, _} = BadJokes.create %{
+  creator: "rafiusky",
+  joke: "Ale &eacute; de Alexandre? Achei que fosse ALE de Abertura Libra Europeia",
+  score: -12
+}
+
+{:ok, _} = BadJokes.create %{
+  creator: "iiBankow",
+  joke: "Ale nao banca.. mas o tiago bankow",
+  score: -5
+}
+
+{:ok, _} = BadJokes.create %{
+  creator: "aleDsz",
+  joke: "Que fazer stand up o que, faz um stand down",
+  score: -7
+}
+
+{:ok, _} = BadJokes.create %{
+  creator: "aleDsz",
+  joke: "CoQUEM?!!?!",
+  score: -7
+}
+
+{:ok, _} = BadJokes.create %{
+  creator: "7x1_",
+  joke: "quando se perde o foco e so da uma ajustada na lente",
+  score: -9
+}
+
+{:ok, _} = BadJokes.create %{
+  creator: "is_equal",
+  joke: "Qual lingugem é mais filosófica? Python (Filósofo Python), pois já dizia que tudo na vida pedende do quanto vc quer comer alguem....",
+  score: -90
+}
+
+{:ok, _} = BadJokes.create %{
+  creator: "looser852",
+  joke: "todo dia é um @7x1_ diferente",
+  score: -8
+}
